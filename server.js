8c8ab4b7cf2500c1eb19f5f1dbeb33ab68b988be
Samson Adegboye <adegboye72@gmail.com>
2020-07-08 05:30:54 +0100

    built for deployment. added server.js for heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+
+const express= require('express');
+const app= express();
+
+app.use(express.static('./dist/laurel-ng'));
+
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/laurel-ng/'});
+});
+
+app.listen(process.env.PORT || 8080);
+
