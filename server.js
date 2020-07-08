6f8d4dc4f1660dfdb3f216afc9491887403a4e55
Samson Adegboye <adegboye72@gmail.com>
2020-07-08 17:30:42 +0100

    solved issues with heroku deployment, pushing this commit to trigger the deployment.

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 
 const express= require('express');
+const path= require('path');
 const app= express();
 
-app.use(express.static('./dist/laurel-ng'));
+app.use(express.static('./dist/laurelng'));
 
 app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/laurel-ng/'});
-}).listen(process.env.PORT || 8080);
+  res.sendFile(path.join('index.html', {root: 'dist/laurelng/'}));
+});
+
+app.listen(process.env.PORT || 8080);
 
